392a0b187399a173771b33212f3e58b41377b2a4
RodrigoGR10 <rodrigo.guerrero.r@ug.uchile.cl>
2026-03-20 12:45:08 -0300

    feat: se implementa el algoritmo de euclides

--- src/main/scala/mcd/euclides.scala
+++ src/main/scala/mcd/euclides.scala
@@ -16,9 +16,28 @@ y luego usarlo como deseen
 import scala.io.StdIn.readInt
 
 @main def euclidesInteractivo(): Unit = {
-  // Inicio de la zona donde deben editar el código
-  
-  println("No le sé al máximo común divisor :(")
-  
-  // Fin de la zona donde deben editar el código
+  var continuar = true
+  while continuar do
+    println("Ingrese el primer número (0 para salir):")
+    val a = readInt()
+    var ra = a
+    if a == 0 then
+      continuar = false
+    else
+      println("Ingrese el segundo número:")
+      var b = readInt()
+      var rb = b
+      if b != 0 then
+        while ra != 0 && rb != 0 do
+          if rb <= ra then
+            ra = ra % rb
+          else
+            rb = rb % ra
+        if ra == 0 then
+          println(s"El MCD de $a y $b es $rb")
+        else
+          println(s"El MCD de $a y $b es $ra")
+      else
+        println("No se puede dividir por 0")
+  println("Adiós!")
 }

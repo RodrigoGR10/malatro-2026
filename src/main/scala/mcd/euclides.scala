5b2556208ebeaa1b333896ea4780ab6797327aec
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-03-23 20:53:19 -0300

    fix: se agregan casos negativos al MCD

--- src/main/scala/mcd/euclides.scala
+++ src/main/scala/mcd/euclides.scala
@@ -16,23 +16,28 @@ y luego usarlo como deseen
 import scala.io.StdIn.readInt
 
 @main def euclidesInteractivo(): Unit = {
-  var continuar = true
-  while continuar do
+  var continuar = true    // variable para definir si el programa continúa
+  while continuar do    // ciclo que dura hasta que el usuario ingresa 0 en el primer número
     println("Ingrese el primer número (0 para salir):")
-    val a = readInt()
-    var ra = a
-    if a == 0 then
+    val a = readInt()    // primer número
+    var ra = a    //variable mutable para aplicar Euclides
+    if a == 0 then    // si es 0, termina el programa
       continuar = false
     else
       println("Ingrese el segundo número:")
-      var b = readInt()
-      var rb = b
-      if b != 0 then
+      var b = readInt()    // segundo valor
+      var rb = b    // variable mutable para aplicar Euclides
+      if ra < 0 then  // si la variable es negativa se multiplica por -1 para dejarla positiva
+        ra = ra * -1
+      if rb < 0 then
+        rb = rb * -1
+      if b != 0 then    // si b es 0, no se puede obtener el MCD. Si no es 0, se realiza el algoritmo de Euclides
         while ra != 0 && rb != 0 do
           if rb <= ra then
             ra = ra % rb
           else
             rb = rb % ra
+        // cuando llega alguna variable mutable a 0, se retorna la otra variable y se obtiene el MCD
         if ra == 0 then
           println(s"El MCD de $a y $b es $rb")
         else

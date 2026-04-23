987a94cb7eed699cad06b8a9f1fc1cab1aba57b8
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-04-22 22:40:12 -0400

    fix: change Nine to case object

--- src/main/scala/rango/Nine.scala
+++ src/main/scala/rango/Nine.scala
@@ -1,7 +1,7 @@
 package cl.uchile.dcc
 package rango
 
-object Nine extends Rank {
+case object Nine extends Rank {
   val orden: Int = 9
   val valor: Int = 9
   val clasificacion: ClasificacionRango = Impar

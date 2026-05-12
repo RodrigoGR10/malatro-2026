a021fac3d773b0c0329b8b8c4f37ef2720122043
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-05-12 19:00:56 -0400

    refactor: replace case class Card with class and add equals

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -1,7 +1,13 @@
 package cl.uchile.dcc
 import pinta.Pinta
 import rango.Rank
-//Representa una carta individual con su rango y pinta
-case class Card(val rank: Rank, val suit: Pinta) {
 
+class Card(val rank: Rank, val suit: Pinta) {
+  override def equals(obj: Any): Boolean = {
+    if !obj.isInstanceOf[Card] then false
+    else {
+      val other = obj.asInstanceOf[Card]
+      this.rank == other.rank && this.suit == other.suit
+    }
+  }
 }

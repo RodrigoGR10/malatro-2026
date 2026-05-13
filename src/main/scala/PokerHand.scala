6b0f95a94cb5fa7aada0e2cebc632c307180b130
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-05-12 20:09:16 -0400

    docs: add Scaladoc to PokerHand

--- src/main/scala/PokerHand.scala
+++ src/main/scala/PokerHand.scala
@@ -1,10 +1,23 @@
 package cl.uchile.dcc
 
-// Determina la mejor combinación posible para una mano de cartas
+/**
+ * Evaluates the best poker combination for a given hand.
+ *
+ * Combinations are evaluated in order of priority from highest to lowest.
+ * The first combination that matches is returned.
+ */
 object PokerHand {
+  /** The list of all combinations ordered from highest to lowest priority. */
   val orderedCombinations: List[PokerCombination] =
     List(StraightFlush, Flush, Straight, ThreeOfAKind, Pair, HighCard)
-
+  /**
+   * Returns the highest-priority combination matched by the hand's cards.
+   *
+   * Always returns at least HighCard since it matches any valid hand.
+   *
+   * @param hand the player's hand to evaluate
+   * @return the best PokerCombination for the given hand
+   */
   def bestCombination(hand: Hand): PokerCombination = {
     var result: PokerCombination = HighCard
     var found = false

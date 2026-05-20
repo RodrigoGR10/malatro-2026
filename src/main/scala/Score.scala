c949902564712b913f82ad791cba766fc76452d1
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-05-20 18:54:04 -0400

    refactor: make Score attributes private, add getters and setters

--- src/main/scala/Score.scala
+++ src/main/scala/Score.scala
@@ -1,24 +1,17 @@
 package cl.uchile.dcc
-/**
- * Represents the score of a poker hand, composed of a chip count and a multiplier.
- *
- * The final score of a hand is computed as chips * mult.
- *
- * @param chips the base chip count of the hand
- * @param mult the multiplier applied to the chip count
- */
-class Score(val chips: Int, val mult: Int) {
-  /**
-   * Returns true if the given object is a Score with the same chips and mult values.
-   *
-   * @param obj the object to compare with
-   * @return true if obj is a Score with equal chips and mult
-   */
+
+class Score(private var _chips: Int, private var _mult: Int) {
+  def chips: Int = _chips
+  def chips_=(value: Int): Unit = _chips = value
+
+  def mult: Int = _mult
+  def mult_=(value: Int): Unit = _mult = value
+
   override def equals(obj: Any): Boolean = {
     if !obj.isInstanceOf[Score] then false
     else {
       val other = obj.asInstanceOf[Score]
-      this.chips == other.chips && this.mult == other.mult
+      this._chips == other._chips && this._mult == other._mult
     }
   }
 }

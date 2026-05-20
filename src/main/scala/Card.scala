71f36ecd34d047221bb9445e533da468e8f919d4
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-05-20 18:54:50 -0400

    refactor: make Card attributes private, add getters and setters

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -2,24 +2,18 @@ package cl.uchile.dcc
 import pinta.Pinta
 import rango.Rank
 
-/**
- * Represents a playing card with a rank and a suit.
- *
- * @param rank the rank of the card
- * @param suit the suit of the card
- */
-class Card(val rank: Rank, val suit: Pinta) {
-  /**
-   * Returns true if this card has the same rank and suit as another object.
-   *
-   * @param obj the object to compare with
-   * @return true if obj is a Card with the same rank and suit
-   */
+class Card(private var _rank: Rank, private var _suit: Pinta) {
+  def rank: Rank = _rank
+  def rank_=(value: Rank): Unit = _rank = value
+
+  def suit: Pinta = _suit
+  def suit_=(value: Pinta): Unit = _suit = value
+
   override def equals(obj: Any): Boolean = {
     if !obj.isInstanceOf[Card] then false
     else {
       val other = obj.asInstanceOf[Card]
-      this.rank == other.rank && this.suit == other.suit
+      this._rank == other._rank && this._suit == other._suit
     }
   }
 }

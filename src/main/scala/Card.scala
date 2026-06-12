a3642720c30aeac92a57181df8b95bc69b1b2424
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-06-12 19:37:46 -0400

    feat: implement Card.applyScore for a list of jokers

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -1,7 +1,7 @@
 package cl.uchile.dcc
 import pinta.Pinta
 import rango.Rank
-import joker._
+import joker.Joker
 
 class Card(private var _rank: Rank, private var _suit: Pinta) {
   def rank: Rank = _rank
@@ -9,6 +9,24 @@ class Card(private var _rank: Rank, private var _suit: Pinta) {
 
   def suit: Pinta = _suit
   def suit_=(value: Pinta): Unit = _suit = value
+
+  /**
+   * Applies this card's contribution to the given score.
+   *
+   * For each joker in the list, applies both the rank's and the
+   * suit's score interaction with that joker.
+   *
+   * @param score  the current score to update
+   * @param jokers the list of active jokers
+   * @return the updated score
+   */
+  def applyScore(score: Score, jokers: List[Joker]): Score = {
+    var current = score
+    for joker <- jokers do
+      current = rank.applyScore(current, joker)
+      current = suit.applyScore(current, joker)
+    current
+  }
 
   override def equals(obj: Any): Boolean = {
     if !obj.isInstanceOf[Card] then false

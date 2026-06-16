3891524ed0d7481dd6c37fb83d8954dcee024ff7
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-06-15 20:16:06 -0400

    fix: DeviousJoker now activates on StraightFlush as well as Straight

--- src/main/scala/joker/DeviousJoker.scala
+++ src/main/scala/joker/DeviousJoker.scala
@@ -1,11 +1,13 @@
 package cl.uchile.dcc
 package joker
-import combinations.{PokerCombination, Straight}
-
-/** Joker that adds +100 chips if the played cards form a Straight. */
+import combinations.{PokerCombination, Straight, StraightFlush}
+/**
+ * Joker that adds +100 chips if the played cards form a Straight
+ * or a Straight Flush, since a Straight Flush is also a Straight.
+ */
 object DeviousJoker extends Joker {
   override def affectCombination(combination: PokerCombination, score: Score): Score = {
-    if combination == Straight then
+    if combination == Straight || combination == StraightFlush then
       score.chips = score.chips + 100
     score
   }
